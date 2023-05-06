d139e44e93cdeb82a021dbeb5fd7f391832a96b1
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-06 13:25:41 +0300

    added 2 routes in app.js

--- nerdshare_frontend/src/App.js
+++ nerdshare_frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Routes, Route, userNavigate } from 'react-router-dom';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='login' element= {<Login />} />,
+      <Route path='/*' element= {<Home />} />,
+    </Routes>
   );
 }
 
