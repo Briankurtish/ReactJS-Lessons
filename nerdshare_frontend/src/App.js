9ed1790ed8c7ff1240f1cfc11570f580fb2f06b2
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-06 13:33:08 +0300

    created the login and Home component

--- nerdshare_frontend/src/App.js
+++ nerdshare_frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route, userNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path='login' element= {<Login />} />,
+      <Route path='login' element={<Login />} />,
       <Route path='/*' element= {<Home />} />,
     </Routes>
   );
